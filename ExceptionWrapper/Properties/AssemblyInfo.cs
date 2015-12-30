bbf3ff4d6dfd84748a506e99fd37f8c487423167
Kaven Gagne <kaven.gagne@gmail.com>
2015-12-30 00:08:25 -0500

    first commit

--- ExceptionWrapper/Properties/AssemblyInfo.cs
+++ ExceptionWrapper/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ExceptionWrapper")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright Kaven Gagne 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
